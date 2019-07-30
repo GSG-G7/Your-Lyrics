33837d3cefaf7fbea44efa36b3e41879c1701617
MaiUbeid <ubead95@gmail.com>
2019-07-30 15:52:35 +0300

    Code controller handlers js Relates #6

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,7 +3,3 @@ const path = require('path');
 exports.get = (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 };
-
-exports.post = (req, res) => {
-  res.redirect('/index.html');
-};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,8 +5,7 @@ const router = express.Router();
 const home = require('./home');
 const error = require('./error');
 
-router.get('/home', home.get);
-router.post('/home', home.post);
+router.get('/', home.get);
 router.use(error.client);
 router.use(error.server);
 
