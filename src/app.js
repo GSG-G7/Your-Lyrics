bc2a7cec5601abba52c4beb6774bb4b7c60c297e
Asmaathabet <asmathabet200@hotmail.com>
2019-07-31 10:30:28 +0300

    make test for route relates # 9

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 const favicon = require('serve-favicon');
 const controllers = require('./controllers/router');
 // Creating our app
@@ -12,6 +13,13 @@ app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 app.use(
   express.static(path.join(__dirname, '..', 'public'), { maxAge: '30d' }),
 );
+// Disable x-powered-by
+app.disable('x-powered-by');
+
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Handling all the routes
 app.use(controllers);
 
--- test/controllers/error.test.js
+++ test/controllers/error.test.js
@@ -1,6 +1,19 @@
 const test = require('tape');
+const supertest = require('supertest');
+const app = require('../../src/app');
 
-test('error test', (t) => {
-  t.equal(3, 3, 'should be 3');
-  t.end();
+test('test for client error 404', (t) => {
+  supertest(app)
+    .get('/error')
+    .expect(404)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+        t.end();
+      } else {
+        t.equal(res.clientError, true, 'should be client error');
+        t.end();
+      }
+    });
 });
