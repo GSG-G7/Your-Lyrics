b42b6de557c3900e2d278a522110891878ad3c9a
asem1789 <as.1789@hotmail.com>
2019-07-31 12:01:02 +0300

    added code for make api request using the request module and handleing the router Relates #13

--- src/app.js
+++ src/app.js
@@ -6,13 +6,10 @@ const controllers = require('./controllers/router');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-// Handling the favicon
 app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
-// Handling the static files
 app.use(
   express.static(path.join(__dirname, '..', 'public'), { maxAge: '30d' }),
 );
-// Handling all the routes
 app.use(controllers);
 
 module.exports = app;
