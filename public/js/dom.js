fa49204708c1731a25483d8e57ac622a1747dfc0
Asmaathabet <asmathabet200@hotmail.com>
2019-07-31 15:53:59 +0300

    make init dom relates #23

---
+++ public/js/dom.js
@@ -0,0 +1,8 @@
+const song = document.querySelector('.home__main__song');
+const search = document.querySelector('.home__main__search');
+const lyrics = document.querySelector('.content__lyrics');
+
+search.addEventListener('click', () => {
+  // lyrics.textContent(object.message.body.lyrics.lyrics_body);
+  console.log('hello');
+});
