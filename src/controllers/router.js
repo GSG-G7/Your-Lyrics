9ae6e4a275377d60902b778a50315c188779b6a4
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-07-31 15:43:51 +0300

    made the fecth in the logic file and the route of the api request Relates #21

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -5,10 +5,11 @@ const router = express.Router();
 const home = require('./home');
 const error = require('./error');
 const request = require('./getRequest');
+const song = require('./song');
 
 router.get('/', home.get);
-router.get('/api-request', request.getRequest);
-
+router.get('/api', request.getRequest);
+router.post('/song', song.post);
 router.use(error.client);
 router.use(error.server);
 
